b8bd241de07e3928e7591268e1c60c64e2cb4160
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-29 17:03:43 +0530

    extract button styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,7 @@ export function Header() {
         <h1 className='text-3xl font-extrabold tracking-tight'>
           <Link to='/'>Noto</Link>
         </h1>
-        <Link
-          to='/create'
-          className='block bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer transition hover:bg-blue-600 font-medium'
-        >
+        <Link to='/create' className='btn'>
           New Note
         </Link>
       </div>
