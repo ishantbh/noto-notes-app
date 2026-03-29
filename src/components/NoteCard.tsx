bf2ecd58f0c01b4d9f6e62c5468f16e8e3418d41
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-29 16:28:05 +0530

    update note card to fix layout issue within card

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -11,9 +11,9 @@ export function NoteCard({ note }: NoteCardProps) {
       to={`/notes/${note.id}`}
       className='block bg-white p-4 rounded-xl shadow-sm transition hover:shadow-md'
     >
-      <div className='flex flex-col gap-1'>
+      <div className='flex flex-col gap-1 h-full'>
         <h2 className='font-bold text-xl'>{note.title}</h2>
-        <p className='line-clamp-2 text-neutral-500 mb-2 flex-1'>
+        <p className='line-clamp-2 text-neutral-500 mb-2 grow'>
           {note.content}
         </p>
         <time dateTime={note.createdAt} className='text-sm text-neutral-400'>
