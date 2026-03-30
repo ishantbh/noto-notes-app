e6db4e709a3e50a6ac562561bda7092a4e8e145f
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-30 16:49:27 +0530

    add back button to edit page

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,7 +1,8 @@
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import { useNotes } from '../hooks/useNotes'
 import { AddEditForm, NoteNotFound } from '../components'
 import type { Note } from '../types'
+import { ArrowLeft } from 'lucide-react'
 
 export default function Edit() {
   let note: Note | undefined
@@ -22,6 +23,8 @@ export default function Edit() {
 }
 
 function EditView({ note }: { note: Note }) {
+  const navigate = useNavigate()
+
   const { updateNote } = useNotes()
 
   function handleSubmit({ title, content }: Omit<Note, 'id' | 'createdAt'>) {
@@ -34,9 +37,18 @@ function EditView({ note }: { note: Note }) {
   return (
     <div className='container mx-auto px-4 py-8'>
       <div>
-        <div className='text-center mb-8'>
-          <h2 className='text-xl font-extrabold'>Edit Note</h2>
-          <p className='text-neutral-500'>Edit an existing note</p>
+        <div className='w-full max-w-xl mx-auto mb-8 gap-4 relative'>
+          <button
+            onClick={() => navigate(-1)}
+            className='absolute btn-outline text-neutral-500 -translate-y-1/2 top-1/2'
+          >
+            <ArrowLeft className='size-5' aria-hidden />
+            <span className='sr-only'>Go Back</span>
+          </button>
+          <div className='text-center'>
+            <h2 className='text-xl font-extrabold'>Edit Note</h2>
+            <p className='text-neutral-500'>Edit an existing note</p>
+          </div>
         </div>
 
         <AddEditForm onSubmit={handleSubmit} note={note} />
