f6ab01a463a35867583d85e51edd9d8ba1270846
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-30 16:45:45 +0530

    add back button to create page

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,8 +1,12 @@
 import { useNotes } from '../hooks/useNotes'
 import { AddEditForm } from '../components'
 import type { Note } from '../types'
+import { ArrowLeft } from 'lucide-react'
+import { useNavigate } from 'react-router'
 
 export default function Create() {
+  const navigate = useNavigate()
+
   const { createNote } = useNotes()
 
   function handleSubmit({ title, content }: Omit<Note, 'id' | 'createdAt'>) {
@@ -15,9 +19,18 @@ export default function Create() {
   return (
     <div className='container mx-auto px-4 py-8'>
       <div>
-        <div className='text-center mb-8'>
-          <h2 className='text-xl font-extrabold'>Create Note</h2>
-          <p className='text-neutral-500'>Create a new note</p>
+        <div className='w-full max-w-xl mx-auto mb-8 gap-4 relative'>
+          <button
+            onClick={() => navigate(-1)}
+            className='absolute btn-outline text-neutral-500 -translate-y-1/2 top-1/2'
+          >
+            <ArrowLeft className='size-5' aria-hidden />
+            <span className='sr-only'>Go Back</span>
+          </button>
+          <div className='text-center'>
+            <h2 className='text-xl font-extrabold'>Create Note</h2>
+            <p className='text-neutral-500'>Create a new note</p>
+          </div>
         </div>
 
         <AddEditForm onSubmit={handleSubmit} />
