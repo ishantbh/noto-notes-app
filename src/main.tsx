79656d95c6ba27e7d43153aebda028c10805e6d7
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-31 16:21:50 +0530

    add theme context

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { NotesProvider } from './context/NotesProvider.tsx'
+import { ThemeProvider } from './context/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NotesProvider>
-      <App />
-    </NotesProvider>
+    <ThemeProvider>
+      <NotesProvider>
+        <App />
+      </NotesProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
