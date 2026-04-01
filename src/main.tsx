2c64a8c935098297f9f610f45ca15f5120f04172
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-01 19:12:43 +0530

    remove notes context

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { NotesProvider } from './context/NotesProvider.tsx'
 import { ThemeProvider } from './context/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <NotesProvider>
-        <App />
-      </NotesProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>,
 )
