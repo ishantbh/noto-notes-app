ae0436f86c65a225dd56c6f720561ab605b5d6ee
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-01 20:03:56 +0530

    cleanup old hook impl commented code

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,7 +2,6 @@ import { Moon, Sun } from 'lucide-react'
 import { useThemeStore } from '../store/useThemeStore'
 
 export function ThemeToggle() {
-  // const { theme, toggleTheme } = useTheme()
   const theme = useThemeStore((state) => state.theme)
   const toggleTheme = useThemeStore((state) => state.toggleTheme)
 
