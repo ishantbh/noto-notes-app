6cb994d94838a18752ffeebe1ad8707b61adeae0
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-03 21:49:22 +0530

    add greeting message on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import { NotesList } from '@/components'
+import { useAuthStore } from '@/store'
+import { extractFirstName } from '@/utils/fomatters'
 
 export default function Home() {
+  const user = useAuthStore((state) => state.user)
+
   return (
     <div className='container mx-auto px-4 py-8'>
+      {user && (
+        <h2 className='text-3xl text-center mb-8'>
+          Welcome{' '}
+          <span className='font-bold'>{extractFirstName(user.name)}!</span>
+        </h2>
+      )}
       <NotesList />
     </div>
   )
