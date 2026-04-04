faa6dcf0491d1a2fe17cbc4a2ecf31442c4f6205
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-04 20:44:25 +0530

    update zustand store

--- src/store/useNotesStore.ts
+++ src/store/useNotesStore.ts
@@ -1,49 +1,36 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 import type { Note } from '@/types'
 
 type NotesState = {
   notes: Record<string, Note>
-  createNote: (note: Omit<Note, 'id' | 'createdAt'>) => void
+  createNote: (note: Note) => void
   updateNote: (note: Note) => void
   deleteNote: (id: string) => void
 }
 
-export const useNotesStore = create<NotesState>()(
-  persist(
-    (set) => ({
-      notes: {},
+export const useNotesStore = create<NotesState>()((set) => ({
+  notes: {},
 
-      createNote: ({ title, content }) => {
-        const newNote: Note = {
-          id: crypto.randomUUID(),
-          title,
-          content,
-          createdAt: new Date().toISOString(),
-        }
+  createNote: (newNote) => {
+    set((state) => ({
+      notes: { ...state.notes, [newNote.id]: newNote },
+    }))
+  },
 
-        set((state) => ({
-          notes: { ...state.notes, [newNote.id]: newNote },
-        }))
+  updateNote: (updatedNote) => {
+    set((state) => ({
+      notes: {
+        ...state.notes,
+        [updatedNote.id]: updatedNote,
       },
+    }))
+  },
 
-      updateNote: (updatedNote) => {
-        set((state) => ({
-          notes: {
-            ...state.notes,
-            [updatedNote.id]: updatedNote,
-          },
-        }))
-      },
+  deleteNote: (id) => {
+    set((state) => {
+      const { [id]: _, ...rest } = state.notes
 
-      deleteNote: (id) => {
-        set((state) => {
-          const { [id]: _, ...rest } = state.notes
-
-          return { notes: rest }
-        })
-      },
-    }),
-    { name: 'notes-storage' },
-  ),
-)
+      return { notes: rest }
+    })
+  },
+}))
